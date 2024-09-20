ba21b46ac45e62d6f6289e0b8000d1818840b24f
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2024-09-20 06:30:23 +0500

    Addition

---
+++ src/app/cancel/page.jsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Payment Canceled</h1>
+      <p>Your booking has not been completed.</p>
+      {/* Provide options to retry or contact support */}
+      {/* div */}
+    </div>
+  );
+};
+
+export default Page;
